981e45d4ceea9afbe6b8e4bcf99a79eb8da698f2
Magnus Dahlstrand <magnus@mdahlstrand.com>
2020-04-01 00:46:30 +0100

    GridScroll prototype: Keyboard adding and selecting

--- src/Prototypes/GridScroll/index.js
+++ src/Prototypes/GridScroll/index.js
@@ -10,12 +10,22 @@ class GridScrollPrototype extends Component {
 				x: 0,
 				y: 0,
 			},
-			nodes: [this.createNewNode({ x: 0, y: 0 })],
+			nodes: [],
 			selection: [],
+		};
+		this.cache = {
+			mouseX: 0,
+			mouseY: 0,
+			$focus: null,
 		};
 		this.$rootEl = createRef();
 		this.handleWheel = this.handleWheel.bind(this);
 		this.handleClick = this.handleClick.bind(this);
+		this.handleKeyDown = this.handleKeyDown.bind(this);
+		this.handleMouseMove = this.handleMouseMove.bind(this);
+	}
+	componentDidMount() {
+		this.$rootEl.current.focus();
 	}
 	handleWheel(event) {
 		const data = {
@@ -26,7 +36,6 @@ class GridScrollPrototype extends Component {
 			movementY: event.movementY,
 		};
 		this.setState(({ origin }) => {
-			// console.log('wheel!', oldState.origin.x + data.deltaX, oldState.origin.y + data.deltaY);
 			return {
 				origin: {
 					x: origin.x - data.deltaX,
@@ -44,29 +53,53 @@ class GridScrollPrototype extends Component {
 			altKey: event.altKey,
 		};
 		if (data.isClickOnGrid) {
-			this.setState(({ origin, nodes }) => {
-				const x = data.pageX - origin.x;
-				const y = data.pageY - origin.y;
-				const newNode = this.createNewNode({ x, y });
-				return {
-					nodes: [...nodes, newNode]
-				}
-			});
+			this.addNewNode({ x: data.pageX, y: data.pageY });
 		} else {
 			const clickedNode = this.state.nodes.find((node) => event.target === node.$el.current);
-			console.log('clicked!', clickedNode);
 			this.selectNode(clickedNode, { extend: data.shiftKey, subtract: data.altKey });
 		}
 	}
-	createNewNode({ x, y }) {
-		return {
-			id: Math.random(),
-			x: x,
-			y: y,
-			w: 300,
-			h: 150,
-			$el: createRef(),
+	handleKeyDown(event) {
+		const data = {
+			which: event.which,
+			charCode: event.charCode,
+			keyCode: event.keyCode,
+			key: event.key,
+			shiftKey: event.shiftKey,
+			altKey: event.altKey,
+		};
+		if (data.key === 't' || data.key === 'T' || data.keyCode === 84) {
+			this.addNewNode({
+				x: this.cache.mouseX,
+				y: this.cache.mouseY,
+			});
 		}
+		if (data.key === 's' || data.key === 'S' || data.keyCode === 83) {
+			const node = this.state.nodes.find((node) => node.$el.current === this.cache.$focus);
+			if (node) {
+				this.selectNode(node, { extend: data.shiftKey, subtract: data.altKey });
+			}
+		}
+	}
+	handleMouseMove(event) {
+		this.cache.mouseX = event.pageX;
+		this.cache.mouseY = event.pageY;
+		this.cache.$focus = event.target;
+	}
+	addNewNode({ x, y }) {
+		this.setState(({ origin, nodes }) => {
+			const newNode = {
+				id: Math.random(),
+				x: x - origin.x,
+				y: y - origin.y,
+				w: 300,
+				h: 150,
+				$el: createRef(),
+			};
+			return {
+				nodes: [...nodes, newNode]
+			}
+		});
 	}
 	selectNode(node, { extend, subtract }) {
 		this.setState(({ selection }) => {
@@ -112,9 +145,12 @@ class GridScrollPrototype extends Component {
 	render() {
 		return (
 			<main
+				tabIndex="0"
 				className={css.root}
 				onWheel={this.handleWheel}
 				onClick={this.handleClick}
+				onKeyDown={this.handleKeyDown}
+				onMouseMove={this.handleMouseMove}
 				ref={this.$rootEl}
 			>
 				{this.renderNodes()}
